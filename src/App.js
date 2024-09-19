4a6ac14662945502a642d16874e1fbebeea74d03
皮喆 <pizhecode@outlook.com>
2024-09-20 00:15:58 +0800

    配置router路由

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { Button } from 'antd';
 
 function App(){
-  return <div>this is my app<Button type='primary'>点我</Button></div>
+  return <div>this is my app<Button type='primary'>点我666</Button></div>
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
