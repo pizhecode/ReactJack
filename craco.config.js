e3cc32c6671d4abf3deceec36bb02cfd4128d4be
皮喆 <pizhecode@outlook.com>
2024-09-28 00:29:30 +0800

    echarts

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,21 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  // webpack 配置
   webpack: {
-    // 配置别名
     alias: {
-      // 约定：使用 @ 表示 src 文件所在路径
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-}
+      '@': path.resolve(__dirname, 'src'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader' // 确保使用的是 Dart Sass
+          ],
+        },
+      ],
+    },
+  },
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,20 @@
-const Home= ()=>{
-    return <div>this is home</div>
+import { BarChart } from '@/pages/Home/components/BarChart'
+
+const Home = () => {
+  return (
+    <div>
+      <BarChart
+        title={'使用满意度'}
+        xData={['Vue', 'React', 'Angular']}
+        sData={[2000, 5000, 1000]} />
+
+      <BarChart
+        title={'使用度'}
+        xData={['Vue', 'React', 'Angular']}
+        sData={[200, 500, 100]}
+        style={{ width: '500px', height: '400px' }} />
+    </div >
+  )
 }
+
 export default Home
