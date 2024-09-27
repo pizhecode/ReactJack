30951eafbb056ec08021f437edd5c0a282c6f29d
皮喆 <pizhecode@outlook.com>
2024-09-27 21:36:04 +0800

    二级路由的配置

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import router from './router'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import 'normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
+import Publish from '@/pages/Publish'
+import Article from '@/pages/Article'
+import Home from '@/pages/Home'
 import AuthRoute from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthRoute><Layout /></AuthRoute>,
+    children: [
+      {
+        // index: true,
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'article',
+        element: <Article />,
+      },
+      {
+        path: 'publish',
+        element: <Publish />,
+      },
+    ]
   },
   {
     path: '/login',
