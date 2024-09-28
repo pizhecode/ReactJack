e361ae0dcc8ad8e3e6edb7bb29b93557c32fb3ac
皮喆 <pizhecode@outlook.com>
2024-09-28 22:52:40 +0800

    频道列表渲染

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import store from './store'
 import 'normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
          <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
 ) 
