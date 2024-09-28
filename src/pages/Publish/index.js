b858984ec2964ba010b2a4a17fa3b1ebab403d92
皮喆 <pizhecode@outlook.com>
2024-09-28 23:22:31 +0800

    上传图片回调

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -3,11 +3,13 @@ import {
     Breadcrumb,
     Form,
     Button,
+    Radio,
     Input,
+    Upload,
     Space,
     Select
-} from 'antd'
-//   import { PlusOutlined } from '@ant-design/icons'
+  } from 'antd'
+  import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import ReactQuill from 'react-quill'
@@ -36,28 +38,47 @@ const Publish = () => {
         //调用接口
         createArticleAPI(reqData)
     }
+    //上传回调
+    const [imageList,setImageList] = useState([])
+    const onChange = (value)=>{
+        // console.log(value);
+        setImageList(value.fileList)
+        
+    }
     return (
         <div className="publish">
             <Card title={<Breadcrumb items={[{ title: <Link to={'/'}>首页</Link> },{ title: '发布文章' },]} /> }>
                 <Form onFinish={onFinish} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }} >
+
                     <Form.Item label="标题" name="title"  rules={[{ required: true, message: '请输入文章标题' }]}>
                         <Input placeholder="请输入文章标题" style={{ width: 400 }} />
                     </Form.Item>
+
                     <Form.Item label="频道" name="channel_id" rules={[{ required: true, message: '请选择文章频道' }]}>
                         <Select placeholder="请选择文章频道" style={{ width: 400 }}>
                             {/* value属性用户选中之后会自动收集起来作为接口的提交字段 */}
                             {channelList.map(item =>(<Option key={item.id} value={item.id}>{item.name}</Option>))}
                         </Select>
                     </Form.Item>
+
+                    <Form.Item label="封面">
+                        <Form.Item name="type">
+                            <Radio.Group><Radio value={1}>单图</Radio><Radio value={3}>三图</Radio><Radio value={0}>无图</Radio></Radio.Group>
+                        </Form.Item>
+                        <Upload onChange={onChange} action={'http://geek.itheima.net/v1_0/upload'} name='image' listType="picture-card" showUploadList><div style={{ marginTop: 8 }}><PlusOutlined /></div></Upload>
+                    </Form.Item>
+
                     <Form.Item label="内容" name="content" rules={[{ required: true, message: '请输入文章内容' }]}>
                         {/* 富文本 */}
                         <ReactQuill className="publish-quill" theme="snow" placeholder="请输入文章内容" />
                     </Form.Item>
+
                     <Form.Item wrapperCol={{ offset: 4 }}>
                         <Space>
                             <Button size="large" type="primary" htmlType="submit">发布文章 </Button>
                         </Space>
                     </Form.Item>
+
                 </Form>
             </Card>
         </div>
