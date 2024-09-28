c3115355f40ed0d88029a5415a401d2d1180059a
皮喆 <pizhecode@outlook.com>
2024-09-29 00:52:39 +0800

    定义状态枚举,管理状态

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -10,12 +10,18 @@ import img404 from '@/assets/error.png'
 import { useChannel } from '@/hooks/useChannel'
 import { useEffect, useState } from 'react'
 import { getArticleAPI } from '@/apis/article'
+import { render } from '@testing-library/react'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
     const { channelList } = useChannel()
+    //定义状态枚举
+    const status = {
+        1: <Tag color='warning'>待审核</Tag>,
+        2: <Tag color="green">审核通过</Tag>
+    }
     const columns = [
         {
           title: '封面',
@@ -33,7 +39,7 @@ const Article = () => {
         {
           title: '状态',
           dataIndex: 'status',
-          render: data => <Tag color="green">审核通过</Tag>
+          render: data => status[data]
         },
         {
           title: '发布时间',
