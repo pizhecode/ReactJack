f217e75a7ca7fb8674a18c3e378451e31dfd62c2
皮喆 <pizhecode@outlook.com>
2024-09-30 19:49:04 +0800

    根据id适配不同状态下的文案

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -90,47 +90,39 @@ const Publish = () => {
             return {url}
            }))
         }
-        getArticleDetail() 
-        //2 通过实例方法 完成回填
+        //如果有id才调用
+        if (articleID) {
+            getArticleDetail()
+         }
     },[articleID,form])
     return (
         <div className="publish">
-            <Card title={<Breadcrumb items={[{ title: <Link to={'/'}>首页</Link> },{ title: '发布文章' },]} /> }>
+            <Card title={<Breadcrumb items={[{ title: <Link to={'/'}>首页</Link> },{ title: `${articleID ? '编辑文章' : '发布文章'}` },]} /> }>
                 <Form form={form} onFinish={onFinish} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 0 }} >
-
                     <Form.Item label="标题" name="title"  rules={[{ required: true, message: '请输入文章标题' }]}>
                         <Input placeholder="请输入文章标题" style={{ width: 400 }} />
                     </Form.Item>
-
                     <Form.Item label="频道" name="channel_id" rules={[{ required: true, message: '请选择文章频道' }]}>
                         <Select placeholder="请选择文章频道" style={{ width: 400 }}>
                             {/* value属性用户选中之后会自动收集起来作为接口的提交字段 */}
-                            {channelList.map(item => (
-        <Option key={item.id} value={item.id}>
-          {item.name}
-        </Option>
-      ))}
+                            {channelList.map(item => (<Option key={item.id} value={item.id}>{item.name} </Option>))}
                         </Select>
                     </Form.Item>
-
                     <Form.Item label="封面">
                         <Form.Item name="type">
                             <Radio.Group onChange={onTypeChange}><Radio value={1}>单图</Radio><Radio value={3}>三图</Radio><Radio value={0}>无图</Radio></Radio.Group>
                         </Form.Item>
                         {imageType > 0 && <Upload maxCount={imageType} multiple={imageType > 1} onChange={onChange} fileList={imageList} action={'http://geek.itheima.net/v1_0/upload'} name='image' listType="picture-card" showUploadList><div style={{ marginTop: 8 }}><PlusOutlined /></div></Upload>}         
                     </Form.Item>
-
                     <Form.Item label="内容" name="content" rules={[{ required: true, message: '请输入文章内容' }]}>
                         {/* 富文本 */}
                         <ReactQuill className="publish-quill" theme="snow" placeholder="请输入文章内容" />
                     </Form.Item>
-
                     <Form.Item wrapperCol={{ offset: 4 }}>
                         <Space>
                             <Button size="large" type="primary" htmlType="submit">发布文章 </Button>
                         </Space>
                     </Form.Item>
-
                 </Form>
             </Card>
         </div>
