47f005257b337ee46a809b97748309a0b4f149b2
皮喆 <pizhecode@outlook.com>
2024-09-30 20:16:08 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
-import Publish from '@/pages/Publish'
-import Article from '@/pages/Article'
-import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+//懒加载
+// import Publish from '@/pages/Publish'
+// import Article from '@/pages/Article'
+// import Home from '@/pages/Home'
 import AuthRoute from '@/components/AuthRoute'
+import { lazy, Suspense } from 'react'
 
+
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
+const Home = lazy(() => import('@/pages/Article'))
 const router = createBrowserRouter([
   {
     path: '/',
@@ -14,15 +20,15 @@ const router = createBrowserRouter([
       {
         index: true,
         // path: 'home',
-        element: <Home />,
+        element:<Suspense fallback={'加载中'}><Home /></Suspense>,
       },
       {
         path: 'article',
-        element: <Article />,
+        element: <Suspense fallback={'加载中'}><Article /></Suspense>,
       },
       {
         path: 'publish',
-        element: <Publish />,
+        element: <Suspense fallback={'加载中'}><Publish /></Suspense>,
       },
     ]
   },
